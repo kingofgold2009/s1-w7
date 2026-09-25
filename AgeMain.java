301f44ee7cf83b8134bdb464cb4f14f2f36e22c7
Lucas Quan <kingofgold2009@gmail.com>
2026-09-25 12:19:19 -0700

    fri 25

--- AgeMain.java
+++ AgeMain.java
@@ -1,6 +1,20 @@
 public class AgeMain{
     public static void main(String[] args) {
-        
+        Age a = new Age();
+        Boolean one = a.isTeenAger(12);
+        System.out.println(one + ": should be false");
+
+       one = a.isTeenAger(13);
+        System.out.println(one + ": should be true"); 
+
+            one = a.isTeenAger(15);
+        System.out.println(one + ": should be true"); 
+
+           one = a.isTeenAger(19);
+        System.out.println(one + ": should be true"); 
+
+           one = a.isTeenAger(22);
+        System.out.println(one + ": should be false"); 
     }
 
 }
--- Fewest.java
+++ Fewest.java
@@ -2,14 +2,14 @@ public class Fewest{
     private String tempStr;
 
     public void fewestComparisons(double temp){
-        if (temp < 40) {
-            tempStr = "cold";
-        } else if (temp >= 40 && temp < 60) {
-            tempStr = "cool";
+        if (temp >= 80) {
+            tempStr = "hot";
         } else if (temp >= 60 && temp < 80) {
             tempStr = "warm";
-        } else if (temp >= 80) {
-            tempStr = "hot";
+        } else if (temp >= 40 && temp < 60) {
+            tempStr = "cool";
+        } else if (temp < 40) {
+            tempStr = "cold";
         }
         System.out.println(tempStr);
     }

62ca8d23af5ea110a56daf45207b869d70ad6be3
Lucas Quan <kingofgold2009@gmail.com>
2026-09-26 15:43:31 +0000

    Hw fixed

--- Stringpractice.java
+++ Stringpractice.java
@@ -1,20 +1,20 @@
 public class Stringpractice{
 
     public String createusername(String first, String last){
-        String a = last.substring(0);
+        String a = last.substring(0,3);
         int b = first.length();
         String c = first.substring(first.length()/2);
         String d = first.substring(0, first.length()/2); //first code if the last name is 3 or less letters
-        String e = last.substring(0,3);
+        String e = last;
 
-        if (last.length() <= 3){
+        if (last.length() >= 3){
             return a + b + c + d; 
         } 
         return e + b + c + d;
        
     }
     public String undoLatin(String word){
-        if(word.length() > 2){
+        if(word.length() < 2){
             return word.substring(0,2);
         }
         return word.substring(2,3) + word.substring(0,2);
@@ -22,6 +22,6 @@ public class Stringpractice{
         
     public String textify (String word, String targetLetter, String replacementCharacter){
     int a = word.indexOf(targetLetter);
-return word.substring(0, a) +  replacementCharacter + word.substring(word.indexOf(a-1));
+return word.substring(0, a) +  replacementCharacter + word.substring(a+1);
     }
 }
--- StringpracticeMain.java
+++ StringpracticeMain.java
@@ -3,5 +3,11 @@ public class StringpracticeMain{
      Stringpractice p = new Stringpractice();
      String a = p.createusername("Lucas", "Quan");
      System.out.println(a);
+     Stringpractice d = new Stringpractice();
+     String b = d.undoLatin("atcay");
+     System.out.println(b);
+     Stringpractice e = new Stringpractice();
+     String c = e.textify("l3t", "3", "e");
+     System.out.println(c);
     }
 }
